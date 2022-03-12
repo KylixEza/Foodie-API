52efeeb3dcbe1383ac5153c09f0e0135313726cf
ACER <loilyxtra@gmail.com>
2022-03-13 02:35:21 +0700

    Initial Commit

---
+++ src/main/kotlin/com/oreyo/Application.kt
@@ -0,0 +1,12 @@
+package com.oreyo
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.oreyo.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/oreyo/data/IFoodieRepository.kt
@@ -0,0 +1,4 @@
+package com.oreyo.data
+
+interface IFoodieRepository {
+}
